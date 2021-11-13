0c9fbc42b72ad48dce14116654fd7ba69cc00d91
Alex Forsythe <awforsythe@gmail.com>
2021-11-13 12:42:21 -0600

    Format a connection string for postgres

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
 #[macro_use] extern crate rocket;
 
+use std::env;
 use rocket::serde::json::Json;
 use serde::{Serialize};
+
+fn var(name: &str, default: &str) -> String {
+    match env::var(name) {
+        Ok(val) => val,
+        Err(_) => default.to_string(),
+    }
+}
+
+fn url() -> String {
+    let db_user = var("DB_USER", "postgres");
+    let db_pass = var("DB_PASS", "");
+    format!("postgresql://{}@{}:{}/{}",
+        if db_pass.is_empty() { db_user } else { format!("{}:{}", db_user, db_pass) },
+        var("DB_HOST", "localhost"),
+        var("DB_PORT", "5432"),
+        var("DB_NAME", "postgres"), // can't pass db_user because it's moved above!
+    )
+}
 
 #[derive(Serialize)]
 struct TestStruct {
@@ -20,5 +39,6 @@ fn index() -> Json<TestStruct> {
 
 #[launch]
 fn rocket() -> _ {
+    println!("{}", url());
     rocket::build().mount("/", routes![index])
 }
